8bbb3ddc9db9c96188527f09b1c61931b2589dca
JoseR <rodri@gmail.com>
2019-09-26 15:27:03 -0500

    Se modificó el código al patrón de diseño CheckStyle

--- src/main/java/jose/rodriguez/everis/peru/app/eureka/SpringBootProyectoEverisEurekaServerApplication.java
+++ src/main/java/jose/rodriguez/everis/peru/app/eureka/SpringBootProyectoEverisEurekaServerApplication.java
@@ -9,8 +9,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class SpringBootProyectoEverisEurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootProyectoEverisEurekaServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootProyectoEverisEurekaServerApplication.class, args);
+  }
 
 }
